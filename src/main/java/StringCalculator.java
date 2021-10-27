fb7e55e8f013d9a20b719262d256ca8338b3f556
goodillich2 <goodillich3@gmail.com>
2021-10-27 22:18:08 +0300

    first commit

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,12 @@
+public class StringCalculator {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int Add(String numbers) {
+        if (numbers.equals("")) {
+            return 0;
+        }
+    }
+}
